228ebf38e13df217c59e06b1ecfbd5aae91bd032
leandrojamir <leandrojamir@hotmail.com>
2023-04-03 16:04:47 -0300

    feat: req5 implement the function get_min_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -18,13 +18,13 @@ def get_max_salary(path: str) -> int:
         # https://acervolima.com/metodo-python-string-isdigit-1/
         if salary.isdigit():
             salary_list.append(int(salary))
-    print(max(salary_list))
-    print(type(salary_list))
-    print(type(salary))
+    # print(max(salary_list))
+    # print(type(salary_list))
+    # print(type(salary))
     return max(salary_list)
 
 
-# tests/test_salaries.py::test_get_max_salary 99860 
+# tests/test_salaries.py::test_get_max_salary 99860
 # <class 'list'>
 # <class 'str'>
 # FAILED
@@ -35,22 +35,25 @@ def get_max_salary(path: str) -> int:
 # JPMorgan Chase &amp;amp; Co	TX	Houston	195818	383416
 
 
+# 5 - Implemente a função get_min_salary
+# Os dados apresentam faixas salariais para cada emprego exibido.
+# Vamos agora encontrar o menor valor de todas as faixas.
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
+    # A função deve obter os dados da mesma forma que o requisito 2.
+    info_file = jobs.read(path)
+    # A função deve retornar um valor inteiro com o menor salário presente
+    # na coluna min_salary.
+    salary_list = []
+    for info in info_file:
+        salary = info["min_salary"]
+        # A função deve ignorar os valores ausentes.
+        if salary.isdigit():
+            salary_list.append(int(salary))
+    return min(salary_list)
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+# tests/test_salaries.py::test_get_max_salary PASSED
+# tests/test_salaries.py::test_get_min_salary PASSED
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
