cfb9deeded34471f10c8a305898468f4032c3cc8
leandrojamir <leandrojamir@hotmail.com>
2023-04-03 10:54:06 -0300

    retomando projeto feat: req1 implement the function read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,23 +1,26 @@
 from functools import lru_cache
 from typing import List, Dict
 
+# 1 - Implemente a função read
+# Para começarmos a processar os dados, devemos antes carregá-los em
+# nossa aplicação. Esta função será responsável por abrir o arquivo
+# CSV e retornar os dados no formato de uma lista de dicionários.
+import csv
+
 
 @lru_cache
+# A função deve receber um path (uma string com o caminho para um arquivo).
 def read(path: str) -> List[Dict]:
-    # iniciando projeto erro set python version
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    # A função deve abrir o arquivo e ler seus conteúdos.
+    with open(path) as file:
+        # A função deve tratar o arquivo como CSV.
+        file = csv.DictReader(file)
+        # A função deve retornar uma lista de dicionários, onde as chaves são
+        # os cabeçalhos de cada coluna e os valores correspondem a cada linha.
+        list = []
+        for row in file:
+            list.append(row)
+    return list
 
 
 def get_unique_job_types(path: str) -> List[str]:
